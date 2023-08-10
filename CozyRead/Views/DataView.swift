2e4fac38e74699c2d5bd8fa67c414b46a333eeaa
Sam Baxter <baxtersa14@gmail.com>
2023-08-10 13:34:34 -0400

    WIP: Editable and searchable data cards

--- CozyRead/Views/DataView.swift
+++ CozyRead/Views/DataView.swift
@@ -10,33 +10,71 @@ import SwiftUI
 
 struct DataView : View {
     @Environment(\.managedObjectContext) var viewContext
+    @Environment(\.editMode) var editMode: Binding<EditMode>?
+    
     @FetchRequest(fetchRequest: BookCSVData.getFetchRequest) var books: FetchedResults<BookCSVData>
-//    @FetchRequest(sortDescriptors: [SortDescriptor(\.dateAdded, order: .reverse)]) var books: FetchedResults<BookCSVData>
+    @State private var searchText: String = ""
     
     var body: some View {
         VStack {
-            List {
-                ForEach(books, id: \.self) { book in
-                    Text(book.title)
+            HStack {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    TextField("Search", text: $searchText)
+                    Spacer()
+                    Image(systemName: "xmark.circle")
+                        .onTapGesture {
+                            searchText.removeAll()
+                        }
                 }
-                .onDelete { offsets in
-                    for offset in offsets {
-                        let book = books[offset]
-                        viewContext.delete(book)
+                .padding(.all, 8)
+                .background(RoundedRectangle(cornerRadius: 10).fill(.black).opacity(0.08))
+                EditButton()
+                    .frame(width: 60)
+            }
+            .padding(.horizontal)
+            
+            TabView {
+                ForEach(books.sorted {$0.year > $1.year}.filter{ (book: BookCSVData) in
+                    book.title.lowercased().hasPrefix(searchText.lowercased()) ||
+                    book.author.lowercased().hasPrefix(searchText.lowercased())
+                }, id: \.self) { (book: BookCSVData) in
+                    DataCardView(book: book)
+                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .always))
+            
+            List {
+                let dict: [Year:[BookCSVData]] = Dictionary(grouping: books, by: {$0.year})
+                ForEach(dict.sorted { $0.key > $1.key }, id: \.key) { (year: Year, books: [BookCSVData]) in
+                    Section(year.description) {
+                        ForEach(books.filter{ (book: BookCSVData) in
+                            book.title.lowercased().hasPrefix(searchText.lowercased()) ||
+                            book.author.lowercased().hasPrefix(searchText.lowercased())
+                        }, id: \.self) { (book: BookCSVData) in
+                            Text(book.title)
+                        }
+                        .onDelete { offsets in
+                            for offset in offsets {
+                                let book = books[offset]
+                                viewContext.delete(book)
+                            }
+                            PersistenceController.shared.save()
+                        }
                     }
-                    PersistenceController.shared.save()
                 }
             }
             Button {
                 let book = BookCSVData(context: viewContext)
                 book.title = "Title"
                 book.author = "Author"
-
+                
                 PersistenceController.shared.save()
             } label: {
                 Label("Add", systemImage: "plus")
             }
         }
+        .background(Color("BackgroundColor"))
     }
 }
 
