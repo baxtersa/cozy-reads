974bcc4e509e0ef9ed10fddd41efefceeccf0832
Sam Baxter <baxtersa14@gmail.com>
2023-08-10 15:35:39 -0400

    WIP: working on multidate selection for daily reading tracking

--- CozyRead/Views/DailyGoalsView.swift
+++ CozyRead/Views/DailyGoalsView.swift
@@ -22,6 +22,7 @@ private struct CheckCircle : View {
                         .inset(by: 2.5)
                         .stroke(style: StrokeStyle(lineWidth: 5))
                         .foregroundColor(.clear)
+                        .contentShape(Circle())
                     Circle()
                         .fill(Gradient(colors: [.blue, .purple]))
                         .transition(.scale)
@@ -62,35 +63,96 @@ private struct CheckCircle : View {
 }
 
 struct DailyGoalsView : View {
+    @Environment(\.managedObjectContext) var viewContext
     @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var daysRead: FetchedResults<ReadingTrackerEntity>
+
     @State private var dates: Set<DateComponents> = []
+    @State private var displayPicker: Bool = false
 
     var body: some View {
         VStack {
-            HStack(spacing: 10) {
-                Spacer()
-                    let fiveDaysAgo = Calendar.current.date(byAdding: .day, value: -4, to: Calendar.current.startOfDay(for: .now)) ?? .now
-                    let lastFiveDays = daysRead.filter { entry in
-                        if let date = entry.date {
-                            return date > fiveDaysAgo
-                        } else {
-                            return false
+            if displayPicker {
+                MultiDatePicker("Reading Tracker", selection: $dates)
+                    .onChange(of: dates, perform: { selection in
+                        let coreDataDates = daysRead.compactMap{$0.date}
+                        let selectedDates = Array(selection.compactMap{$0.date})
+
+                        print("onChange: ", selectedDates, coreDataDates)
+
+                        let diff = selectedDates.difference(from: coreDataDates)
+                        print("diff: ", diff)
+                        
+                        for change in diff.insertions + diff.removals{
+                            switch change {
+                            case .insert(offset: _, element: let element, associatedWith: _):
+                                do {
+                                    print("adding ", element)
+                                    let entry = ReadingTrackerEntity(context: viewContext)
+                                    entry.date = element
+                                }
+                            case .remove(offset: _, element: let element, associatedWith: _):
+                                if let entry = daysRead.first(where: { entry in
+                                    entry.date == element
+                                }) {
+                                    print("deleting ", entry.date as Any)
+                                    viewContext.delete(entry)
+                                } else {
+                                    print("failed to remove ", element)
+                                }
+                            }
                         }
+                    })
+                    .onAppear {
+                        dates = Set(daysRead.compactMap{$0.date}.map{Calendar.current.dateComponents([.day, .month, .year], from: $0)})
+
+                        let coreDataDates = daysRead.compactMap{$0.date}
+                        let selectedDates = Array(dates.compactMap{$0.date})
+                        
+                        print("onAppear: ", selectedDates, coreDataDates)
                     }
-                ForEach(0..<5) { days in
-                    let date = Calendar.current.date(byAdding: .day, value: days, to:   fiveDaysAgo)
-                    let entry = lastFiveDays.first { entry in
-                        entry.date == date
+                Divider()
+                    Button {
+                        withAnimation {
+                            displayPicker.toggle()
+                        }
+                    } label: {
+                        Text("Done")
                     }
-                    CheckCircle(entry: entry, date: date!)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding([.trailing])
+            } else {
+                VStack {
+                    HStack(spacing: 10) {
+                        Spacer()
+                        let fiveDaysAgo = Calendar.current.date(byAdding: .day, value: -4, to: Calendar.current.startOfDay(for: .now)) ?? .now
+                        let lastFiveDays = daysRead.filter { entry in
+                            if let date = entry.date {
+                                return date >= fiveDaysAgo
+                            } else {
+                                return false
+                            }
+                        }
+                        ForEach(0..<5) { days in
+                            let date = Calendar.current.date(byAdding: .day, value: days, to:   fiveDaysAgo)
+                            let entry = lastFiveDays.first { entry in
+                                entry.date == date
+                            }
+                            CheckCircle(entry: entry, date: date!)
+                        }
+                        Spacer()
+                    }
+                    let days = daysInARow
+                    let daysText = days == 1 ? "day" : "days"
+                    Text("read \(daysInARow) \(daysText) in a row")
+                        .padding(.top)
+                        .italic()
                 }
-                Spacer()
+                .onTapGesture {
+                    withAnimation {
+                        displayPicker.toggle()
+                    }
+                }
             }
-            let days = daysInARow
-            let daysText = days == 1 ? "day" : "days"
-            Text("read \(daysInARow) \(daysText) in a row")
-                .padding(.top)
-                .italic()
         }
         .scaledToFit()
         .padding(.vertical)
