a5141d2d0fbbeb6188fe1bc93edc58a4fdc9b12d
Sam Baxter <baxtersa14@gmail.com>
2023-08-10 15:34:14 -0400

    WIP: More editable data cards

--- CozyRead/Views/DataCardView.swift
+++ CozyRead/Views/DataCardView.swift
@@ -10,37 +10,49 @@ import SwiftUI
 
 struct DataCardView : View {
     @Environment(\.editMode) private var editMode: Binding<EditMode>?
-    
+
     let book: BookCSVData
     private let editableBinding: Binding<Int>
+    private var isEditing: Bool {
+        editMode?.wrappedValue.isEditing == true
+    }
     
     init(book: BookCSVData) {
         self.book = book
         self.editableBinding = Binding(get: {book.rating}, set: {book.rating = $0})
     }
-    
+
     var body: some View {
         HStack {
             Spacer()
             VStack(spacing: 10) {
-                if editMode?.wrappedValue.isEditing == true {
+                if isEditing {
                     let titleBinding = Binding(get: { book.title }, set: { title in
                         book.title = title
                     })
                     let authorBinding = Binding(get: { book.author }, set: { author in
                         book.author = author
                     })
+                    let genreBinding = Binding(get: { book.genre }, set: { genre in
+                        print(genre)
+                        book.setGenre(genre)
+                    })
                     
                     TextField(book.title, text: titleBinding)
                         .font(.system(.title))
                         .multilineTextAlignment(.center)
                         .transition(.identity)
-                    HStack {
-                        TextField(book.author, text: authorBinding)
-                            .font(.system(.title3))
-                            .italic()
-                            .multilineTextAlignment(.center)
+                    TextField(book.author, text: authorBinding)
+                        .font(.system(.title3))
+                        .italic()
+                        .multilineTextAlignment(.center)
+
+                    Picker("Genre", selection: genreBinding) {
+                        ForEach(Genre.allCases) { genre in
+                            Text(genre.rawValue)
+                        }
                     }
+                    .tint(.white)
                 } else {
                     Text(book.title)
                         .font(.system(.title))
@@ -49,27 +61,69 @@ struct DataCardView : View {
                     Text("by \(book.author)")
                         .font(.system(.title3))
                         .italic()
+                    Text(book.genre.rawValue)
                 }
                 
-                if book.year == .tbr || book.year == .reading {
-                    Spacer()
+                Spacer()
+                if book.year == .tbr {
+                    if isEditing {
+                        Button {
+                            book.setYear(.reading)
+                            book.dateStarted = Date.now
+                        } label: {
+                            Label("To Be Read", systemImage: "circle")
+                        }
+                    } else {
+                        Label("To Be Read", systemImage: "circle")
+                    }
+                } else if book.year == .reading {
+                    if isEditing {
+                        Button {
+                            if let year = Calendar.current.dateComponents([.year], from: Date.now).year {
+                                book.setYear(.year(year))
+                                book.dateCompleted = Date.now
+                            }
+                        } label: {
+                            Label("Currently Reading", systemImage: "ellipsis.circle")
+                        }
+                    } else {
+                        Label("Currently Reading", systemImage: "ellipsis.circle")
+                    }
                 } else {
-                    let ratingBinding =
-                        editMode?.wrappedValue.isEditing == true ?
-                        editableBinding :
-                        Binding.constant(book.rating)
+                    let ratingBinding = isEditing ? editableBinding : Binding.constant(book.rating)
                     
-                    Spacer()
                     StarRating(rating: ratingBinding)
                         .ratingStyle(SolidRatingStyle(color: .white))
+
                     Spacer()
-                    
                     HStack {
-                        Label("Finished", systemImage: "checkmark.circle")
-                            .symbolVariant(book.year == .tbr ? .none : .fill)
-                        if let dateCompleted: Date = book.dateCompleted {
-                            let _ = dateCompleted.formatted(date: .numeric, time: .omitted)
-                            Text("Complete: \(dateCompleted.formatted(date: .numeric, time: .omitted))")
+                        if isEditing {
+                            Button {
+                                book.setYear(.tbr)
+                                book.dateCompleted = nil
+                            } label: {
+                                Label("Finished", systemImage: "checkmark.circle")
+                            }
+
+                            let dateBinding = Binding(
+                                get: {
+                                    book.dateCompleted ?? Date.now
+                                },
+                                set: { date in
+                                    book.dateCompleted = date
+                                    if let started = book.dateStarted,
+                                       date < started {
+                                        book.dateStarted = nil
+                                    }
+                                })
+                            DatePicker("", selection: dateBinding, displayedComponents: .date)
+                                .datePickerStyle(.automatic)
+                                .colorScheme(.dark)
+                        } else {
+                            Label("Finished", systemImage: "checkmark.circle")
+                            if let dateCompleted: Date = book.dateCompleted {
+                                Text("\(dateCompleted.formatted(date: .numeric, time: .omitted))")
+                            }
                         }
                     }
                 }
@@ -86,7 +140,7 @@ struct DataCardView : View {
 }
 
 struct DataCardView_Previews : PreviewProvider {
-    static private var book = try! BookCSVData(from: [
+    static private var finishedBook = try! BookCSVData(from: [
         "Title": "The Long Way to a Small, Angry Planet",
         "Author": "Becky Chambers",
         "Genre": "Sci-fi",
@@ -94,9 +148,21 @@ struct DataCardView_Previews : PreviewProvider {
         "Year": "2023",
         "Rating": "4"
     ], context: PersistenceController.preview.container.viewContext)
+    static private var tbrBook = try! BookCSVData(from: [
+        "Title": "To Be Taught, If Fortunate",
+        "Author": "Becky Chambers",
+        "Genre": "Sci-fi",
+        "DateAdded": "7/1/2023",
+        "Year": "TBR"
+    ], context: PersistenceController.preview.container.viewContext)
+    
+    @Environment(\.editMode) static private var editMode
 
     static var previews: some View {
-        DataCardView(book: book)
-            .environment(\.editMode, Binding.constant(EditMode.active))
+        VStack {
+            EditButton()
+            DataCardView(book: finishedBook)
+            DataCardView(book: tbrBook)
+        }
     }
 }
--- CozyRead/Views/DataView.swift
+++ CozyRead/Views/DataView.swift
@@ -14,18 +14,30 @@ struct DataView : View {
     
     @FetchRequest(fetchRequest: BookCSVData.getFetchRequest) var books: FetchedResults<BookCSVData>
     @State private var searchText: String = ""
+    @State private var selectedCard: Int = 0
     
     var body: some View {
+        let dict: [Year:[BookCSVData]] = Dictionary(grouping: books, by: {$0.year})
+        let booksByYear = dict.sorted { $0.key > $1.key }.map { (year: Year, books: [BookCSVData]) in
+            books.filter{ (book: BookCSVData) in
+                book.title.lowercased().hasPrefix(searchText.lowercased()) ||
+                book.author.lowercased().hasPrefix(searchText.lowercased())
+            }
+        }
+
         VStack {
             HStack {
                 HStack {
                     Image(systemName: "magnifyingglass")
                     TextField("Search", text: $searchText)
                     Spacer()
-                    Image(systemName: "xmark.circle")
-                        .onTapGesture {
-                            searchText.removeAll()
-                        }
+                    if !searchText.isEmpty {
+                        Image(systemName: "xmark.circle")
+                            .onTapGesture {
+                                searchText.removeAll()
+//                                selectedCard = 0
+                            }
+                    }
                 }
                 .padding(.all, 8)
                 .background(RoundedRectangle(cornerRadius: 10).fill(.black).opacity(0.08))
@@ -34,15 +46,56 @@ struct DataView : View {
             }
             .padding(.horizontal)
             
-            TabView {
-                ForEach(books.sorted {$0.year > $1.year}.filter{ (book: BookCSVData) in
-                    book.title.lowercased().hasPrefix(searchText.lowercased()) ||
-                    book.author.lowercased().hasPrefix(searchText.lowercased())
-                }, id: \.self) { (book: BookCSVData) in
-                    DataCardView(book: book)
+            ZStack {
+                TabView(selection: $selectedCard) {
+                    var index = 0
+                    //                ForEach(booksByYear, id: \.self) { books in
+                    //                    ForEach(books, id: \.self) { book in
+                    //                        DataCardView(book: book)
+                    //                            .tag(index)
+                    //                        let _ = index += 1
+                    //                    }
+                    //                }
+                    ForEach(books.sorted {$0.year > $1.year}.filter{ (book: BookCSVData) in
+                        book.title.lowercased().hasPrefix(searchText.lowercased()) ||
+                        book.author.lowercased().hasPrefix(searchText.lowercased())
+                    }, id: \.self) { (book: BookCSVData) in
+                        DataCardView(book: book)
+                            .tag(index)
+                        let _ = index += 1
+                    }
+                }
+                .onChange(of: selectedCard, perform: { value in
+                    print(value)
+                })
+                .tabViewStyle(.page(indexDisplayMode: .always))
+                let numItems = booksByYear.compactMap{$0.count}.reduce(0, +)
+                if numItems > 1 {
+                    HStack {
+                        Button {
+                            selectedCard = max(0, selectedCard - 1)
+                        } label: {
+                            Image(systemName: "chevron.left")
+                                .bold()
+                        }
+                        .tint(.white)
+                        .padding(.leading)
+                        .buttonStyle(.bordered)
+                        
+                        Spacer()
+                        
+                        Button {
+                            selectedCard = min(numItems - 1, selectedCard + 1)
+                        } label: {
+                            Image(systemName: "chevron.right")
+                                .bold()
+                        }
+                        .tint(.white)
+                        .padding(.trailing)
+                        .buttonStyle(.bordered)
+                    }
                 }
             }
-            .tabViewStyle(.page(indexDisplayMode: .always))
             
             List {
                 let dict: [Year:[BookCSVData]] = Dictionary(grouping: books, by: {$0.year})
