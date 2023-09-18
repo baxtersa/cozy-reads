33b97cdb86205f8491728a1fc6aaea45b6f32ee2
Sam Baxter <baxtersa14@gmail.com>
2023-09-18 16:59:08 -0400

    Fix site html char encoding

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -10,10 +10,11 @@ export default function Home() {
             <Navbar />
             <Hero />
             <SectionTitle pretitle="CozyReads Features" title="What you get with CozyReads">
-                CozyReads is a reading habit tracker with a personal touch. Set goals, track
-                your progress, and visualize your taste in books without the pressure of a
-                social media community and comparison culture. Customize tags and filter graphs
-                however you wish. Plan out your "To Read" list so that you are always looking
+                CozyReads is a reading habit tracker with a personal touch. Set
+                goals, track your progress, and visualize your taste in books
+                without the pressure of a social media community and comparison
+                culture. Customize tags and filter graphs however you wish. Plan
+                out your &quot;To Read&quot; list so that you are always looking
                 forward to your next book.
            </SectionTitle>
             <Features data={featureDevices} imgPos='right' />
