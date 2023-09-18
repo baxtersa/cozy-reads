7d8f1ebbf6c1b34ce85f0cb159e5165389a2d3fe
Sam Baxter <baxtersa14@gmail.com>
2023-09-18 16:29:10 -0400

    Fix next config to serve css assets

--- docs/next.config.js
+++ docs/next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     images: {
         loader: 'custom',
         loaderFile: './src/image-loader.ts',
-    }
+    },
+    assetPrefix: '/cozy-reads/'
 }
 
 module.exports = nextConfig
